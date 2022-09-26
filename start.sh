f582e52e2439e366c35c05aee925702c284f1f06
Godstimep21 <104645270+Godstimep21@users.noreply.github.com>
2022-09-26 12:12:29 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone https://github.com/Godstimep21/joeyking.git
 fi
-cd /EvaMaria
+cd /joeyking
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
